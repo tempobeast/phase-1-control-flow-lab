1cbb961c44b8e150f31407fea018afbc4ba6ee31
Zach <shaw.zacht@gmail.com>
2022-02-22 14:51:55 -0800

    commit tree 25e3c9cd0ed3780fd7fbd75b803788d4354dc6c0

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400) {
+    return 'This one is on me!'
+  } else if (rideLength >= 2000 && rideLength <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (rideLength > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(quality){
+ switch(quality) {
+   case 'generous' :
+     return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    case 'thanks for everything' :
+      return 'Bye.'
+ } 
 }
